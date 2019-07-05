862e1c8df94f46bc7f828533b9d86fea4cfcfb65
waketzheng <waketzheng@gmail.com>
2019-07-05 20:33:25 +0800

    add latest django version to setup.py

--- setup.py
+++ setup.py
@@ -60,6 +60,7 @@ setup(
         "Framework :: Django",
         "Framework :: Django :: 2.0",
         "Framework :: Django :: 2.1",
+        "Framework :: Django :: 2.2",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
